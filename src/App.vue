279b51fd8621c376ce25810fc722aa64d6ba7a83
Mohammed Propelius <mohammed.kharodawala@propelius.tech>
2023-03-28 17:36:13 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HarryPotterCards from "./components/HarryPotterCards.vue";
+</script>
+
+<template>
+ <main>
+   <h1>Hero</h1>
+   <HarryPotterCards/>
+ </main>
+</template>
+
+<style scoped>
+
+</style>
